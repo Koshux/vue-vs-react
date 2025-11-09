4457b15ec5aeac2e9c6ef74dc70fe4fc6aef6cc8
James <lanzonprojects@gmail.com>
2025-11-09 22:45:07 +0100

    fix(react-app): fixed unit tests config for react
    React vitest config was not excluding playwright tests. We have to use the vitest/config exclude
    rules to ensure that within the vitest.config.ts file.

--- packages/react-app/vitest.config.ts
+++ packages/react-app/vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -7,6 +7,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './vitest.setup.ts',
+    exclude: [...configDefaults.exclude, 'tests/e2e/**'],
     coverage: {
       reporter: ['text', 'html'],
     },
